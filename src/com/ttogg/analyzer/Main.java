33ec4ee69885bba8a60cb015805edecde09958af
Ruslan Onashko <ruslan.onashko@gmail.com>
2022-05-17 03:11:23 +0300

    Добавлен метод bruteForce

--- src/com/ttogg/analyzer/Main.java
+++ src/com/ttogg/analyzer/Main.java
@@ -17,6 +17,10 @@ public class Main {
             {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п',
                     'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
                     '.', ',', '"', ':', '-', '!', '?', ' '};
+    private static final char[] SYMBOL_ALPHABET =
+            {'.', ','};
+    private static final char[] VOWEL_ALPHABET =
+            {'а', 'е', 'и', 'о', 'у', 'э', 'ы', 'ю', 'я'};
     private static final String ORIGINAL_FILE = "Enter original file (*.txt):";
     private static final String DESTINATION_FILE = "Enter destination file (*.txt):";
     private static final String ADDITIONAL_FILE = "Enter example file (*.txt):";
@@ -32,8 +36,8 @@ public class Main {
  *       В данный момент взаимодействие с пользователем происходит через консоль, это методы:
  *       askKey(), askFile(), askMode(), askSomething().
  *       Планирую перенести их в другой класс.
- *       Правильно ли я понимаю, что нужно создать class UserInterface с public методами
- *       для получения информации от пользователя?
+ *       Правильно ли я понимаю, что нужно создать class UserInterface с public static методами
+ *       для получения информации от пользователя (если не делать на JavaFX или Swing)?
  */
 
             int mode = askMode(scanner);
@@ -67,11 +71,14 @@ public class Main {
 //                case 4:
 //                    System.out.println("Not implemented yet");
 //            }
+//
 
-
+/*
+*           Было бы удобно сохранять эти Path в поля класса, но это казалось плохим решением, так ли это?
+* */
 
             Path originalFile = askFile(scanner, ORIGINAL_FILE, true);
-            Path destinationFile = askFile(scanner, DESTINATION_FILE, false);
+            Path destinationFile = askFile(scanner, DESTINATION_FILE, false);  // Сделать проверку на отличие исходного файла и конечного.
 
             switch (mode) {
                 case 1:
@@ -83,7 +90,7 @@ public class Main {
                 case 3:
                     key = bruteForce(originalFile);
                     break;
-                case 4:
+                default:
                     System.out.println("Not implemented yet");
             }
 
@@ -91,51 +98,87 @@ public class Main {
         }
     }
 
-/*
- *   Проверка для моего тестового файла, пока не реализовано
- * */
-    private static boolean checkKey(char[] chars, int key) {
-        if (newChar(chars[2], key) == 'д') {
-            return true;
-        } else {
-            return false;
+    /*
+     *       А есть ли другой вариант проверки наличия элемента в алфавите? Не вижу смысла char[] во что-то преобразовывать.
+     *       Была идея преобразовать char[] в TreeSet, но будет ли это быстрее работать для этого размера алфавита,
+     *       либо есть другие причины использовать TreeSet, или другой подход?
+     * */
+    private static int indexInAlphabet(char c, char[] alphabet) {
+        int index = -1;
+        for (int i = 0; i < alphabet.length; i++) {
+            if (alphabet[i] == c) {
+                index = i;
+                break;
+            }
         }
+        return index;
     }
 
-/*
- *   Метод пока не реализован
- * */
+    private static double checkKeyBruteForce(char[] chars, int charCount, int key) {
+        int spaceCount = 0;
+        int symbolCheckCount = 0;
+
+        char[] newChars = new char[charCount];
+
+        for (int i = 0; i < charCount - 1; i++) {
+            newChars[i] = newChar(chars[i], key);
+        }
+
+        for (int i = 0; i < charCount - 1; i++) {
+            if (newChars[i] == ' ') {
+                spaceCount++;
+            }
+            if (indexInAlphabet(newChars[i], SYMBOL_ALPHABET) >= 0 && newChars[i + 1] == ' ') {
+                symbolCheckCount++;
+            }
+        }
+
+        double result = 100.0 * spaceCount / charCount + 700.0 * symbolCheckCount/charCount;
+
+        return result;
+    }
+
     private static int bruteForce(Path originalFile) {
-        int key = 0;
-        int charCount;
+        int originalCharCount = 0;
 /*
  *       Как вариант для хранения части файла для обработки кроме массива, либо создания временных файлов
  *       ничего не придумал (есть еще ByteStream, CharStream, но есть ли смысл их использовать
  *       не знаю, и не совсем понимаю как ими пользоваться)
- *       Массив ниже имеет размер 2*10_000_000 = 20 Мбайт, если не ошибаюсь, что не много.
+ *       Массив ниже имеет размер 2*1_000_000 = 2 Мбайт, если не ошибаюсь, что не много.
  *       При этом не уверен, что размера достаточно для анализа правильности подбора ключа.
  * */
-        char[] chars = new char[10_000_000];
+
+        char[] originalChars = new char[1_000_000];
 
         try (BufferedReader reader = Files.newBufferedReader(originalFile, StandardCharsets.UTF_8)) {
-            for (key = 0; key < ALPHABET.length; key++) {
-                charCount = 0;
-                while (reader.ready() && charCount < chars.length) {
-                    chars[charCount++] = (char) reader.read();
-                }
-                if (checkKey(chars, -key)) {
-                    return -key;
-                }
+            while (reader.ready() && originalCharCount < originalChars.length) {
+                originalChars[originalCharCount++] = (char) reader.read();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return key;                     // Если не нашло ключ - создает копию оригинального файла
+
+        TreeMap<Integer, Double> checking = new TreeMap<>();
+
+        for (int key = 0; key < ALPHABET.length; key++) {
+            double resultOfCheck = checkKeyBruteForce(originalChars, originalCharCount, -key);
+            checking.put(-key, resultOfCheck);
+        }
+
+        int bestKey = 0;
+        double bestValue = -1;
+        for (Map.Entry<Integer, Double> entry : checking.entrySet()) {
+            if (entry.getValue() > bestValue) {
+                bestValue = entry.getValue();
+                bestKey = entry.getKey();
+            }
+        }
+        return bestKey;
     }
 
     private static void converting(Path originalFile, Path destinationFile, int key) {
 /*
- *       Здесь и далее используется конструкция:
+ *       Здесь и других местах используется конструкция:
  *       try (BufferedReader reader = Files.newBufferedReader(originalFile, StandardCharsets.UTF_8)) {
  *           ...
  *       } catch (IOException e) {
@@ -156,21 +199,9 @@ public class Main {
     }
 
     private static char newChar(char odlChar, int key) {
-        int newCharNumber;
-        boolean isInAlphabet = false;
-/*
- *       А есть ли другой вариант проверки наличия элемента в алфавите? Не вижу смысла char[] во что-то преобразовывать.
- *       Была идея преобразовать char[] в TreeSet, но будет ли это быстрее работать для этого размера алфавита,
- *       либо есть другие причины использовать TreeSet, или другой подход?
- * */
-        for (newCharNumber = 0; newCharNumber < ALPHABET.length; newCharNumber++) {
-            if (ALPHABET[newCharNumber] == odlChar) {
-                isInAlphabet = true;
-                break;
-            }
-        }
+        int newCharNumber = indexInAlphabet(odlChar, ALPHABET);
 
-        if (!isInAlphabet) {
+        if (newCharNumber < 0) {
             return odlChar;
         } else {
             newCharNumber += key;
