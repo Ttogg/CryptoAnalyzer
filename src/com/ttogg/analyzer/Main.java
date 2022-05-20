a955d7a1f0d8e4e976246e150d5085461290c85f
Ruslan Onashko <ruslan.onashko@gmail.com>
2022-05-20 04:07:27 +0300

    Добавлен метод statisticalAnalysis

--- src/com/ttogg/analyzer/Main.java
+++ src/com/ttogg/analyzer/Main.java
@@ -19,82 +19,96 @@ public class Main {
                     '.', ',', '"', ':', '-', '!', '?', ' '};
     private static final char[] SYMBOL_ALPHABET =
             {'.', ','};
-    private static final char[] VOWEL_ALPHABET =
-            {'а', 'е', 'и', 'о', 'у', 'э', 'ы', 'ю', 'я'};
     private static final String ORIGINAL_FILE = "Enter original file (*.txt):";
     private static final String DESTINATION_FILE = "Enter destination file (*.txt):";
-    private static final String ADDITIONAL_FILE = "Enter example file (*.txt):";
+    private static final String EXAMPLE_FILE = "Enter example file (*.txt):";
     private static final String FILE_IS_EXIST = "File exists. Do you want to rewrite it? (y/n)";
     private static final String FILE_NOT_EXIST = "File not exists";
-    private static final String MODE = "Enter mode (1 - Encryption; 2 - Decryption; 3 - BruteForce):";
+    private static final String ORIGINAL_DESTINATION = "Original file and destination file must be different.";
+    private static final String MODE = "Enter mode (1 - Encryption; 2 - Decryption; 3 - BruteForce; 4 - Statistical Analysis):";
     private static final String KEY = "Enter key 0.." + (ALPHABET.length - 1) + ":";
 
+    private static Path originalFile;
+    private static Path destinationFile;
+    private static Path exampleFile;
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         try (Scanner scanner = new Scanner(System.in)) {
-/*
- *       В данный момент взаимодействие с пользователем происходит через консоль, это методы:
- *       askKey(), askFile(), askMode(), askSomething().
- *       Планирую перенести их в другой класс.
- *       Правильно ли я понимаю, что нужно создать class UserInterface с public static методами
- *       для получения информации от пользователя (если не делать на JavaFX или Swing)?
- */
+            /*
+             *       В данный момент взаимодействие с пользователем происходит через консоль, это методы:
+             *       askKey(), askFile(), askMode(), askSomething().
+             *       Планирую перенести их в другой класс.
+             *       Правильно ли я понимаю, что нужно создать class UserInterface с public static методами
+             *       для получения информации от пользователя (если не делать на JavaFX или Swing)?
+             */
 
             int mode = askMode(scanner);
             int key = 0;
 
-//            Это здесь для удобства моего тестирования
-//
-//            D:\Java\Projects\Files\OriginalFile.txt
-//            D:\Java\Projects\Files\EncryptedFile.txt
-//            D:\Java\Projects\Files\DecryptedFile.txt
-//            D:\Java\Projects\Files\BruteForceFile.txt
-//            final Path ORIGINAL = Path.of("D:\\Java\\Projects\\Files\\OriginalFile.txt");
-//            final Path ENCRYPTED = Path.of("D:\\Java\\Projects\\Files\\EncryptedFile.txt");
-//            final Path DECRYPTED = Path.of("D:\\Java\\Projects\\Files\\DecryptedFile.txt");
-//            final Path BRUTE = Path.of("D:\\Java\\Projects\\Files\\BruteForceFile.txt");
-//
-//
-//            switch (mode) {
-//                case 1:
-//                    key = askKey(scanner);
-//                    converting(ORIGINAL, ENCRYPTED, key);
-//                    break;
-//                case 2:
-//                    key = -askKey(scanner);
-//                    converting(ENCRYPTED, DECRYPTED, key);
-//                    break;
-//                case 3:
-//                    key = bruteForce(ENCRYPTED);
-//                    converting(ENCRYPTED, BRUTE, key);
-//                    break;
-//                case 4:
-//                    System.out.println("Not implemented yet");
-//            }
-//
-
-/*
-*           Было бы удобно сохранять эти Path в поля класса, но это казалось плохим решением, так ли это?
-* */
-
-            Path originalFile = askFile(scanner, ORIGINAL_FILE, true);
-            Path destinationFile = askFile(scanner, DESTINATION_FILE, false);  // Сделать проверку на отличие исходного файла и конечного.
+            /*
+             *       Тут плохое решение, но я не придумал лучше
+             * */
 
             switch (mode) {
                 case 1:
+                    askFiles(scanner, ORIGINAL_FILE, DESTINATION_FILE);
                     key = askKey(scanner);
                     break;
                 case 2:
+                    askFiles(scanner, ORIGINAL_FILE, DESTINATION_FILE);
                     key = -askKey(scanner);
                     break;
                 case 3:
+                    askFiles(scanner, ORIGINAL_FILE, DESTINATION_FILE);
                     key = bruteForce(originalFile);
                     break;
+                case 4:
+                    askFiles(scanner, ORIGINAL_FILE, DESTINATION_FILE, EXAMPLE_FILE);
+                    key = statisticalAnalysis(originalFile, exampleFile);
+                    break;
                 default:
-                    System.out.println("Not implemented yet");
+                    System.out.println("Something went wrong");
+                    System.exit(-1);
             }
 
             converting(originalFile, destinationFile, key);
+        }
+    }
+
+    private static void converting(Path originalFile, Path destinationFile, int key) throws IOException {
+        /*
+         *       Здесь и других местах используется конструкция:
+         *       try (BufferedReader reader = Files.newBufferedReader(originalFile, StandardCharsets.UTF_8)) {
+         *           ...
+         *       } catch (IOException e) {
+         *           e.printStackTrace();
+         *       }
+         *       Не уверен, что это грамотное решение.
+         * */
+        try (BufferedReader reader = Files.newBufferedReader(originalFile, StandardCharsets.UTF_8);
+             BufferedWriter writer = Files.newBufferedWriter(destinationFile, StandardCharsets.UTF_8)) {
+
+            while (reader.ready()) {
+                char c = newChar((char) reader.read(), key);
+                writer.write(c);
+            }
+        }
+    }
+
+    private static char newChar(char odlChar, int key) {
+        int newCharNumber = indexInAlphabet(odlChar, ALPHABET);
+
+        if (newCharNumber < 0) {
+            return odlChar;
+        } else {
+            newCharNumber += key;
+            if (newCharNumber > ALPHABET.length - 1) {
+                newCharNumber -= ALPHABET.length;
+            } else if (newCharNumber < 0) {
+                newCharNumber += ALPHABET.length;
+            }
+            return ALPHABET[newCharNumber];
         }
     }
 
@@ -112,6 +126,127 @@ public class Main {
             }
         }
         return index;
+    }
+
+    private static int statisticalAnalysis(Path encryptedFile, Path exampleFile) throws IOException {
+        int encryptedCount = 0;
+        int exampleCount = 0;
+        int maxLengthExample = 1_000_000;
+
+        char[] encryptedChars = new char[1_000_000];
+        try (BufferedReader reader = Files.newBufferedReader(encryptedFile, StandardCharsets.UTF_8)) {
+            while (reader.ready() && encryptedCount < encryptedChars.length) {
+                encryptedChars[encryptedCount++] = (char) reader.read();
+            }
+        }
+
+        TreeMap<Character, Double> exampleFrequencies = new TreeMap<>();
+        for (char c : ALPHABET) {
+            exampleFrequencies.put(c, 0.0);
+        }
+        try (BufferedReader reader = Files.newBufferedReader(exampleFile, StandardCharsets.UTF_8)) {
+
+            char mapKey;
+            while (reader.ready() && exampleCount < maxLengthExample) {
+                mapKey = (char) reader.read();
+                if (exampleFrequencies.containsKey(mapKey)) {
+                    exampleFrequencies.put(mapKey, exampleFrequencies.get(mapKey) + 1);
+                    exampleCount++;
+                }
+            }
+        }
+
+        if (exampleCount == 0) {
+            return 0;
+        }
+        for (Map.Entry<Character, Double> entry : exampleFrequencies.entrySet()) {
+            entry.setValue(entry.getValue() / exampleCount * encryptedCount);
+        }
+
+
+        TreeMap<Integer, Double> checking = new TreeMap<>();
+        for (int key = 0; key < ALPHABET.length; key++) {
+            double resultOfCheck = checkKeyStatisticalAnalysis(encryptedChars, encryptedCount, -key, exampleFrequencies);
+            checking.put(-key, resultOfCheck);
+        }
+        int bestKey = 0;
+        double bestValue = Double.MAX_VALUE;
+        for (Map.Entry<Integer, Double> entry : checking.entrySet()) {
+            if (entry.getValue() < bestValue) {
+                bestValue = entry.getValue();
+                bestKey = entry.getKey();
+            }
+        }
+        return bestKey;
+    }
+
+    private static double checkKeyStatisticalAnalysis(char[] encryptedChars, int encryptedCharCount, int key, TreeMap<Character, Double> exampleFrequencies) {
+        double xi = 0;
+
+        char[] newChars = new char[encryptedCharCount];
+        for (int i = 0; i < encryptedCharCount; i++) {
+            newChars[i] = newChar(encryptedChars[i], key);
+        }
+
+        TreeMap<Character, Double> encryptedFrequencies = new TreeMap<>();
+        for (char c : ALPHABET) {
+            encryptedFrequencies.put(c, 0.0);
+        }
+        char mapKey;
+        for (int i = 0; i < encryptedCharCount; i++) {
+            mapKey = newChars[i];
+            if (encryptedFrequencies.containsKey(mapKey)) {
+                encryptedFrequencies.put(mapKey, encryptedFrequencies.get(mapKey) + 1);
+            }
+        }
+
+        char currentKey;
+        double curEnValue;
+        double curExValue;
+        for (Map.Entry<Character, Double> entry : exampleFrequencies.entrySet()) {
+            currentKey = entry.getKey();
+            curEnValue = encryptedFrequencies.get(currentKey);
+            curExValue = exampleFrequencies.get(currentKey);
+            if (curExValue >= 1) {
+                xi += (curEnValue - curExValue) * (curEnValue - curExValue) / curExValue;
+            }
+        }
+
+        return xi;
+    }
+
+    private static int bruteForce(Path originalFile) throws IOException {
+        int originalCharCount = 0;
+        /*
+         *       Как вариант для хранения части файла для обработки кроме массива, либо создания временных файлов
+         *       ничего не придумал (есть еще ByteStream, CharStream, но есть ли смысл их использовать
+         *       не знаю, и не совсем понимаю как ими пользоваться)
+         *       Массив ниже имеет размер 2*1_000_000 = 2 Мбайт, если не ошибаюсь, что не много.
+         *       При этом не уверен, что размера достаточно для анализа правильности подбора ключа.
+         * */
+
+        char[] originalChars = new char[1_000_000];
+        try (BufferedReader reader = Files.newBufferedReader(originalFile, StandardCharsets.UTF_8)) {
+            while (reader.ready() && originalCharCount < originalChars.length) {
+                originalChars[originalCharCount++] = (char) reader.read();
+            }
+        }
+
+        TreeMap<Integer, Double> checking = new TreeMap<>();
+        for (int key = 0; key < ALPHABET.length; key++) {
+            double resultOfCheck = checkKeyBruteForce(originalChars, originalCharCount, -key);
+            checking.put(-key, resultOfCheck);
+        }
+
+        int bestKey = 0;
+        double bestValue = -1;
+        for (Map.Entry<Integer, Double> entry : checking.entrySet()) {
+            if (entry.getValue() > bestValue) {
+                bestValue = entry.getValue();
+                bestKey = entry.getKey();
+            }
+        }
+        return bestKey;
     }
 
     private static double checkKeyBruteForce(char[] chars, int charCount, int key) {
@@ -133,85 +268,7 @@ public class Main {
             }
         }
 
-        double result = 100.0 * spaceCount / charCount + 700.0 * symbolCheckCount/charCount;
-
-        return result;
-    }
-
-    private static int bruteForce(Path originalFile) {
-        int originalCharCount = 0;
-/*
- *       Как вариант для хранения части файла для обработки кроме массива, либо создания временных файлов
- *       ничего не придумал (есть еще ByteStream, CharStream, но есть ли смысл их использовать
- *       не знаю, и не совсем понимаю как ими пользоваться)
- *       Массив ниже имеет размер 2*1_000_000 = 2 Мбайт, если не ошибаюсь, что не много.
- *       При этом не уверен, что размера достаточно для анализа правильности подбора ключа.
- * */
-
-        char[] originalChars = new char[1_000_000];
-
-        try (BufferedReader reader = Files.newBufferedReader(originalFile, StandardCharsets.UTF_8)) {
-            while (reader.ready() && originalCharCount < originalChars.length) {
-                originalChars[originalCharCount++] = (char) reader.read();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        TreeMap<Integer, Double> checking = new TreeMap<>();
-
-        for (int key = 0; key < ALPHABET.length; key++) {
-            double resultOfCheck = checkKeyBruteForce(originalChars, originalCharCount, -key);
-            checking.put(-key, resultOfCheck);
-        }
-
-        int bestKey = 0;
-        double bestValue = -1;
-        for (Map.Entry<Integer, Double> entry : checking.entrySet()) {
-            if (entry.getValue() > bestValue) {
-                bestValue = entry.getValue();
-                bestKey = entry.getKey();
-            }
-        }
-        return bestKey;
-    }
-
-    private static void converting(Path originalFile, Path destinationFile, int key) {
-/*
- *       Здесь и других местах используется конструкция:
- *       try (BufferedReader reader = Files.newBufferedReader(originalFile, StandardCharsets.UTF_8)) {
- *           ...
- *       } catch (IOException e) {
- *           e.printStackTrace();
- *       }
- *       Не уверен, что это грамотное решение.
- * */
-        try (BufferedReader reader = Files.newBufferedReader(originalFile, StandardCharsets.UTF_8);
-             BufferedWriter writer = Files.newBufferedWriter(destinationFile, StandardCharsets.UTF_8)) {
-
-            while (reader.ready()) {
-                char c = newChar((char) reader.read(), key);
-                writer.write(c);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static char newChar(char odlChar, int key) {
-        int newCharNumber = indexInAlphabet(odlChar, ALPHABET);
-
-        if (newCharNumber < 0) {
-            return odlChar;
-        } else {
-            newCharNumber += key;
-            if (newCharNumber > ALPHABET.length - 1) {
-                newCharNumber -= ALPHABET.length;
-            } else if (newCharNumber < 0) {
-                newCharNumber += ALPHABET.length;
-            }
-            return ALPHABET[newCharNumber];
-        }
+        return 100.0 * spaceCount / charCount + 700.0 * symbolCheckCount / charCount;
     }
 
     private static int askMode(Scanner scanner) {
@@ -229,34 +286,69 @@ public class Main {
         }
     }
 
-    private static Path askFile(Scanner scanner, String fileType, boolean isSource) {
-        String path;
+    private static void askFiles(Scanner scanner, String type1, String type2) throws IOException {
         while (true) {
-            System.out.println(fileType);
-            path = askSomething(scanner);
-            Path file = new File(path).toPath();
-
-            boolean ends = path.endsWith(".txt");
-            if (!ends) {
+            System.out.println(type1);
+            String path1 = askSomething(scanner);
+            if (!path1.endsWith(".txt")) {
                 continue;
             }
-            if (isSource) {
-                if (Files.isRegularFile(file)) {
-                    return file;
-                } else {
-                    System.out.println(FILE_NOT_EXIST);
+
+            Path file1 = new File(path1).toPath();
+            if (Files.isRegularFile(file1)) {
+                originalFile = file1;
+                break;
+            } else {
+                System.out.println(FILE_NOT_EXIST);
+            }
+        }
+
+        while (true) {
+            System.out.println(type2);
+            String path2 = askSomething(scanner);
+            if (!path2.endsWith(".txt")) {
+                continue;
+            }
+
+            Path file2 = new File(path2).toPath();
+            if (Files.isSameFile(originalFile, file2)) {
+                System.out.println(ORIGINAL_DESTINATION);
+                continue;
+            }
+            if (Files.exists(file2) && Files.isRegularFile(file2)) {
+                System.out.println(FILE_IS_EXIST);
+                if (askSomething(scanner).equals("y")) {
+                    destinationFile = file2;
+                    break;
                 }
             } else {
-                if (Files.exists(file) && Files.isRegularFile(file)) {
-                    System.out.println(FILE_IS_EXIST);
-                    if (askSomething(scanner).equals("y")) {
-                        return file;
-                    }
-                } else {
-                    if (Files.isDirectory(file.getParent())) {
-                        return file;
-                    }
+                if (Files.isDirectory(file2.getParent())) {
+                    destinationFile = file2;
+                    break;
                 }
+            }
+        }
+    }
+
+    private static void askFiles(Scanner scanner, String type1, String type2, String type3) throws IOException {
+        askFiles(scanner, type1, type2);
+        while (true) {
+            System.out.println(type3);
+            String path1 = askSomething(scanner);
+            if (!path1.endsWith(".txt")) {
+                continue;
+            }
+
+            Path file3 = new File(path1).toPath();
+            if (Files.isSameFile(destinationFile, file3)) {
+                System.out.println(ORIGINAL_DESTINATION);
+                continue;
+            }
+            if (Files.isRegularFile(file3)) {
+                exampleFile = file3;
+                break;
+            } else {
+                System.out.println(FILE_NOT_EXIST);
             }
         }
     }
